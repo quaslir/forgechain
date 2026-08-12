92483d478bf0e55b7c19c881ab10d16519784baa
quaslir <karlnechai@icloud.com>
2026-08-12 17:22:14 +0200

    Add OpenSSL-based SHA-256 implementation and update app targets

--- include/crypto/Hash.hpp
+++ include/crypto/Hash.hpp
@@ -1,5 +1,25 @@
+#pragma once
 #include <cstdint>
 #include <array>
+#include <vector>
+#include <openssl/evp.h>
+#include <memory>
+#include <string>
 namespace forgechain::crypto {
-using HashBytes = std::array<uint32_t, 32>;
+    struct EVPMDCTXDeleter {
+      void operator()(EVP_MD_CTX *ptr) const {
+        if (ptr)
+          EVP_MD_CTX_free(ptr);
+      }
+    };
+using HashBytes = std::array<uint8_t, 32>;
+using bytes = std::vector<uint8_t>;
+using str = std::string;
+using unique_evp_md_ctx = std::unique_ptr<EVP_MD_CTX, EVPMDCTXDeleter>;
+
+
+
+HashBytes sha256(const bytes& data);
+HashBytes double_sha_256(const bytes& data);
+str to_hex(const HashBytes& data);
 }
--- src/crypto/Hash.cpp
+++ src/crypto/Hash.cpp
@@ -1,1 +1,23 @@
 #include "crypto/Hash.hpp"
+#include <openssl/evp.h>
+
+#include <stdexcept>
+namespace forgechain::crypto {
+HashBytes sha256(const bytes& data) {
+    HashBytes hash;
+    unique_evp_md_ctx ctx(EVP_MD_CTX_new());
+     if(!ctx) {
+         throw std::runtime_error("EVP_MD_CTX_new failed");
+     }
+
+     unsigned int out_len = 0;
+
+     if(EVP_DigestInit_ex(ctx.get(), EVP_sha256(), nullptr) != 1 ||
+         EVP_DigestUpdate(ctx.get(), data.data(), data.size()) != 1 ||
+         EVP_DigestFinal_ex(ctx.get(), hash.data(), &out_len) != 1) {
+              throw std::runtime_error("SHA-256 computation failed");
+         }
+
+     return hash;
+}
+}
